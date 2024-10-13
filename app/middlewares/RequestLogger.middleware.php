d16d8cc48773a441b5458df34cc85f496f86bb4d
Mert Öztürk <imertt29@gmail.com>
2024-10-13 20:52:50 +0300

    request logger updated

--- app/middlewares/RequestLogger.middleware.php
+++ app/middlewares/RequestLogger.middleware.php
@@ -6,11 +6,15 @@ class RequestLoggerMiddleware extends BaseMiddleware
 {
     public function handle($response = null)
     {
-        $currentDate = $this->getCurrentDate();
-        $requestMethod = $_SERVER['REQUEST_METHOD'];
-        $requestUri = $_SERVER['REQUEST_URI'];
-        $logMessage = "[$currentDate] $requestMethod $requestUri\n";
+        if (isset($_SERVER['HTTP_USER_AGENT']) && $_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
+            $currentDate = $this->getCurrentDate();
+            $requestMethod = $_SERVER['REQUEST_METHOD'];
+            $requestUri = $_SERVER['REQUEST_URI'];
+            $logMessage = "[$currentDate] $requestMethod $requestUri\n";
 
-        $this->logRequest($logMessage);
+            $this->logRequest($logMessage);
+        }
+
+        // return $next($request);
     }
 }
