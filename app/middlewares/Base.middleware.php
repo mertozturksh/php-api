4fda2ca405ba4643175d040a7a68c14805ee1848
Mert Öztürk <imertt29@gmail.com>
2024-10-14 15:50:51 +0300

    base middleware updated

--- app/middlewares/Base.middleware.php
+++ app/middlewares/Base.middleware.php
@@ -5,23 +5,4 @@ namespace App\Middlewares;
 abstract class BaseMiddleware
 {
     abstract public function handle($request, $next);
-
-
-    
-    protected function logRequest($logMessage)
-    {
-        $logDirectory = __DIR__ . '/../../logs';
-        $logFile = $logDirectory . '/request.log';
-
-        if (!is_dir($logDirectory)) {
-            mkdir($logDirectory, 0755, true); // Create the directory with proper permissions
-        }
-
-        file_put_contents($logFile, $logMessage, FILE_APPEND);
-    }
-
-    protected function getCurrentDate()
-    {
-        return date('Y-m-d H:i:s');
-    }
 }
--- app/middlewares/RequestLogger.middleware.php
+++ app/middlewares/RequestLogger.middleware.php
@@ -7,7 +7,7 @@ class RequestLoggerMiddleware extends BaseMiddleware
     public function handle($request, $next)
     {
         if (isset($_SERVER['HTTP_USER_AGENT']) && $_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
-            $currentDate = $this->getCurrentDate();
+            $currentDate = date('Y-m-d H:i:s');
             $requestMethod = $_SERVER['REQUEST_METHOD'];
             $requestUri = $_SERVER['REQUEST_URI'];
             $logMessage = "[$currentDate] $requestMethod $requestUri\n";
@@ -16,5 +16,17 @@ class RequestLoggerMiddleware extends BaseMiddleware
         }
 
         return $next($request);
+    }
+
+    private function logRequest($logMessage)
+    {
+        $logDirectory = __DIR__ . '/../../logs';
+        $logFile = $logDirectory . '/request.log';
+
+        if (!is_dir($logDirectory)) {
+            mkdir($logDirectory, 0755, true); // Create the directory with proper permissions
+        }
+
+        file_put_contents($logFile, $logMessage, FILE_APPEND);
     }
 }
